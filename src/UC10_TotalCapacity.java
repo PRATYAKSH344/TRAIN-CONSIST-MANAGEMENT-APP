2a88af101e44878af82a388e2cdc7fe213f4b8e0
PRATYAKSH344 <pratyakshrana028@gmail.com>
2026-04-07 10:36:09 +0530

    UC10: Calculate total seating capacity using Stream reduce

--- src/UC10_TotalCapacity.java
+++ src/UC10_TotalCapacity.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 // Reusing Bogie class
 class Bogie {
@@ -25,7 +24,7 @@ class Bogie {
     }
 }
 
-public class UC9_GroupBogies {
+public class UC10_TotalCapacity {
     public static void main(String[] args) {
 
         // Step 1: Create list (reuse from UC7)
@@ -34,20 +33,13 @@ public class UC9_GroupBogies {
         bogies.add(new Bogie("AC Chair", 78));
         bogies.add(new Bogie("First Class", 50));
         bogies.add(new Bogie("General", 90));
-        bogies.add(new Bogie("Sleeper", 65));
 
-        // Step 2: Group using Stream API
-        Map<String, List<Bogie>> groupedBogies = bogies.stream()
-                .collect(Collectors.groupingBy(Bogie::getName));
+        // Step 2: Map + Reduce
+        int totalCapacity = bogies.stream()
+                .map(Bogie::getCapacity)
+                .reduce(0, Integer::sum);
 
-        // Step 3: Display grouped data
-        System.out.println("Grouped Bogies:");
-
-        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-            System.out.println("\nCategory: " + entry.getKey());
-            for (Bogie b : entry.getValue()) {
-                System.out.println(b);
-            }
-        }
+        // Step 3: Display result
+        System.out.println("Total Seating Capacity: " + totalCapacity);
     }
 }
